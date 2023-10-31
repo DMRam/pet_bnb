e09832d4814a86ab21790c91660a741e47afc5d0
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2023-10-31 16:10:50 -0400

    ClientController updated

--- src/main/java/com/ulogic/it/petbnb/demo/controller/users/ClientController.java
+++ src/main/java/com/ulogic/it/petbnb/demo/controller/users/ClientController.java
@@ -1,10 +1,18 @@
 package com.ulogic.it.petbnb.demo.controller.users;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ulogic.it.petbnb.demo.model.users.Client;
+import com.ulogic.it.petbnb.demo.service.users.ClientDataServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /**
  * <p>
+ * This controller Class is taking care of CRUD request for Clients
  * </p>
  *
  * @author dannymunoz on 2023-10-23
@@ -13,4 +21,62 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/client")
 public class ClientController {
+
+    @Autowired
+    ClientDataServices clientDataServices;
+
+    /**
+     * <p>
+     * Getting Clients
+     * </p>
+     *
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @GetMapping("all")
+    public ResponseEntity<List<Client>> getClients() throws ExecutionException, InterruptedException {
+        return new ResponseEntity<>(clientDataServices.getClientsData().get(), HttpStatus.OK);
+    }
+
+    /**
+     * <p>
+     * Create Client
+     * </p>
+     *
+     * @param client
+     */
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addClient(@RequestBody Client client) {
+        clientDataServices.addClientData(client);
+    }
+
+    /**
+     * <p>
+     * Delete Client
+     * </p>
+     *
+     * @param id
+     */
+    @DeleteMapping("/delete/")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteClient(@RequestParam String id) {
+        clientDataServices.deleteClientById(id);
+    }
+
+    /**
+     * <p>
+     * Update Client
+     * </p>
+     *
+     * @param client
+     */
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateClient(@RequestBody Client client) {
+        clientDataServices.updateClientById(client);
+    }
+
+
 }
